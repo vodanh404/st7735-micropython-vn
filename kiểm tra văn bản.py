8bc74e36b8f0d3814ec134a10882c4c1c7fee271
Đinh Viết Phúc <159451818+vodanh404@users.noreply.github.com>
2025-08-26 20:55:51 +0700

    Update kiểm tra văn bản.py

--- kiểm tra văn bản.py
+++ kiểm tra văn bản.py
@@ -1,6 +1,6 @@
 from machine import SPI,Pin
 from st7735.viet_font import viet_font_12x20
-from st7735 import TFT   # driver bạn đã chỉnh
+from st7735 import TFT   
 
 spi = SPI(1, baudrate=20000000, polarity=0, phase=0, sck=Pin(26), mosi=Pin(27), miso=None)
 dc = 21
